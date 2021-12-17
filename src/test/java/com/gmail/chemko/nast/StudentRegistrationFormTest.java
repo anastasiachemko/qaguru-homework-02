d029b763e906f013a589d83f8de43b365e2dd69c
anastasiachemko <nast.chemko@gmail.com>
2021-12-17 22:40:15 +0300

    added

--- src/test/java/com/gmail/chemko/nast/StudentRegistrationFormTest.java
+++ src/test/java/com/gmail/chemko/nast/StudentRegistrationFormTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -20,24 +21,36 @@ public class StudentRegistrationFormTest {
     void successTest() {
         open("https://demoqa.com/automation-practice-form");
 
-        $("firstName").setValue("Anastasia");
+        $("#firstName").setValue("Anastasia");
         $("#lastName").setValue("Chemko");
         $("#userEmail").setValue("nast.chemko@gmail.com");
         $("[for='gender-radio-2']").click();
         $("#userNumber").setValue("1117508498");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("October");
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__day--010:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("English").pressEnter();
         $("[for='hobbies-checkbox-1']").click();
+        $("#uploadPicture").uploadFromClasspath("rafting.jpg");
         $("#currentAddress").setValue("Minsk");
-        $("#submit").click();
+        $("#state").scrollTo().click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
+        $("#submit").scrollTo().click();
 
-        $("#output").shouldBe(visible);
-        $("#firstName").shouldHave(text("Anastasia"));
-        $("#lastName").shouldHave(text("Chemko"));
-        $("#userEmail").shouldHave(text("nast.chemko@gmail.com"));
-        $("#gender").shouldHave(text("Female"));
-        $("#userNumber").shouldHave(text("1117508498"));
-        $("#subjects").shouldHave(text("English"));
-        $("#hobbies").shouldHave(text("Sports"));
-        $("#currentAddress").shouldHave(text("Minsk"));
+        $("#example-modal-sizes-title-lg").shouldBe(visible);
+        $(".table-responsive").shouldHave(text("Anastasia"));
+        $(".table-responsive").shouldHave(text("Chemko"));
+        $(".table-responsive").shouldHave(text("nast.chemko@gmail.com"));
+        $(".table-responsive").shouldHave(text("Female"));
+        $(".table-responsive").shouldHave(text("1117508498"));
+        $(".table-responsive").shouldHave(text("English"));
+        $(".table-responsive").shouldHave(text("Sports"));
+        $(".table-responsive").shouldHave(text("rafting.jpg"));
+        $(".table-responsive").shouldHave(text("Minsk"));
+        $(".table-responsive").shouldHave(text("Haryana"));
+        $(".table-responsive").shouldHave(text("Karnal"));
     }
 }
